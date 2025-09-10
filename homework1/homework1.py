2eb9199a104f15dbcb8438cbd251ed385f4d537d
rw369 <riley_wood@berkeley.edu>
2025-09-10 14:36:41 -0700

    ss of creating hw1, and 3.1 of hw1

---
+++ homework1/homework1.py
@@ -0,0 +1,67 @@
+#File: homework1. py
+
+# --- Variables and Data Types --- 
+
+a = 10
+b = 1.5
+c = 3j
+d = "hello"
+e = [1,2,3]
+f = {"name": "Ellen", "favorite fruit": "strawberry"}
+g = (1,2)
+h = ["apple", "banana", "strawberry"]
+i = True
+j = None
+k = [True, "blue", 12]
+l = str(14)
+m = 1e4
+"""
+1.) 9 different data types
+2.) Integery, Float, Complex, String, List, Dictionary, Tuple, Boolean, and NoneType
+3.) b-m, d-l, e-h-k
+4.) l is not an integer because the str() command converts whatever is in the () to a string
+5.) Another data type is a set, which is used to store multiple items in a single variable, and it is
+a collection that is unordered, unindexed, and you cannot change set items, but you can add and remove set items
+"""
+
+
+print(a)
+print(type(a)) #a is an integer, which is a whole number, no decimals
+
+print(b)
+print(type(b)) #b is a float, which is a number with decimals
+
+print(c)
+print(type(c)) #c is a complex, which is a number consisting of a real part and imaginary part
+
+print(d)
+print(type(d)) #d is a string, which is a sequence of characters in quotations
+
+print(e)
+print(type(e)) #e is a list, which is a data structure storing multiple items into a single variable
+
+print(f)
+print(type(f)) #f is a dictionary, which is a datat structure that stores values in key: value pairs
+
+print(g)
+print(type(g)) #g is a tuple, which stores multiple items in one variable like a list, but the elements of a tuple cannot be changed like a list
+
+print(h)
+print(type(h)) #h is a list, which is a data structure storing multiple items into a single variable
+
+print(i)
+print(type(i)) #i is a boolean, which is a value that is either true or false
+
+print(j)
+print(type(j)) #j is a NoneType, which is the data type for the None object, which indicates no value
+
+print(k)
+print(type(k)) #k is a list, which is a data structure storing multiple items into a single variable
+
+print(l)
+print(type(l)) #l is a string, which is a sequence of characters in quotations
+
+print(m)
+print(type(m)) #m is a float, which is a number with decimals
+
+
